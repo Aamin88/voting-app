5e543eece7e7291a2ab71c9b048564fb537ac790
Aamin88 <forkahamin@yahoo.co.uk>
2024-08-27 11:01:39 +0100

    add: vercel config to redirect all route to /

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import "./home.css";
-
+import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="landing__page section__padding">
       <h1 className="section__heading-text">Coming soon</h1>
+      <Link to={"/login"}>Login</Link>
     </div>
   );
 }
