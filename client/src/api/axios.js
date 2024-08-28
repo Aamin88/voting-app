3b22964797d335a97f385394801912af6e88d01a
Aamin88 <forkahamin@yahoo.co.uk>
2024-08-28 11:14:59 +0100

    update: remove withCredential option from axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const baseURL = "https://voting-app-r8ql.vercel.app";
-// const baseURL: "http://localhost:5001";
+// const baseURL = "https://voting-app-r8ql.vercel.app";
+const baseURL = "http://localhost:5001";
 
 export default axios.create({
   baseURL,
-  withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
