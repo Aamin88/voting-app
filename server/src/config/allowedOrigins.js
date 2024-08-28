9852eef934b79969334cf365f323d695f4d706af
Amin <forkahamin@yahoo.co.uk>
2024-08-28 07:30:17 +0100

    Update allowedOrigins.js
    adding client side url to cors allowed list

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -6,6 +6,8 @@ const allowedOrigin = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5500",
+  "https://voting-app-ten-gold.vercel.app",
+  ""https://voting-9h7iwp3c3-aamin887s-projects.vercel.app
 ];
 
 module.exports = allowedOrigin;
