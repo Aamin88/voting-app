769addec3b5b3b71e5a002216b2adf9cfed9805f
Aamin88 <forkahamin@yahoo.co.uk>
2024-08-28 10:37:53 +0100

    fixed: urls in allow url cors list

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -7,7 +7,7 @@ const allowedOrigin = [
   "http://localhost:5174",
   "http://127.0.0.1:5500",
   "https://voting-app-ten-gold.vercel.app",
-  ""https://voting-9h7iwp3c3-aamin887s-projects.vercel.app
+  "https://voting-9h7iwp3c3-aamin887s-projects.vercel.app",
 ];
 
 module.exports = allowedOrigin;
